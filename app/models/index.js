5a84f20a461e8381455bcd5cb39b1012755ee607
Arunodaya Singh <arunodaya.singh.611@gmail.com>
2022-09-26 10:46:11 +0530

    server updates

--- app/models/index.js
+++ app/models/index.js
@@ -7,7 +7,7 @@ const db = {};
 db.mongoose = mongoose;
 
 db.user = require("./user.model");
-db.role = require("..role.model");
+db.role = require("./role.model");
 
 db.ROLES = ["user", "admin"];
 
