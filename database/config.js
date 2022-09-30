402cb6bcf417c9fccf2505c41f604a3238e96092
Arunodaya Singh <arunodaya.singh.611@gmail.com>
2022-09-30 11:30:07 +0530

    new uot

--- database/config.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27107/testing")
-.then(() => console.log("Database connected!"))
-.catch(err => console.log(err));;
--- server.js
+++ server.js
@@ -1,74 +1,24 @@
 const express = require("express");
-const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 
-var corsOptions = {
-    origin : "http://localhost:8081"
-};
 
-app.use(cors(corsOptions));
+const connectDB = async()=>{
 
-app.use(express.json());
+    mongoose.connect('mongodb://localhost:27017/testing');
+    const userSchema = new mongoose.Schema({});
+    const users = mongoose.model('users',userSchema);
+    const data = await users.find();
+    console.warn(data);
+}
 
-app.use(express.urlencoded({extended: true}));
-
-const db = require("./app/models/");
-<<<<<<< HEAD
-const Role = db.Role;
-
-
-
-
-
-=======
-// const Role = db.role;
-
-// db.mongoose
-//     .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`,{
-//         useNewUrlParser : true,
-//         useUnifiedTopology: true
-//     })
-//     .then(() =>{
-//         console.log("Successfully connected to Mongo Database");
-//         initial();
-//     })
-//     .catch(err =>{
-//         console.error("connection error", err);
-//         process.exit();
-//     });
- 
->>>>>>> 5a84f20a461e8381455bcd5cb39b1012755ee607
-
-app.get("/", (req,res) =>{
-    res.json({ message : "Welcome To login api application"});
-});
+connectDB();
 
 
 // setting port 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port : ${PORT}.`)
-<<<<<<< HEAD
 });
-=======
-});
-
-
-
-// function initial(){
-//     Role.estimatedDocumentCount((err, count) =>{
-//         if(!err && count === 0){
-//             new Role({
-//                 name:"user"
-//             }).save(err=>{
-//                 if(err) {
-//                     console.log("error",err);
-//                 }
-//                 console.log("added 'user' to roles collection");
-//             })
-//         }
-//     })
-// }
->>>>>>> 5a84f20a461e8381455bcd5cb39b1012755ee607
